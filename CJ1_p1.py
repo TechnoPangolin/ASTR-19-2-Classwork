3a9f6a6645bebde36638568ecc578f710c6e83fb
Cobannus\TechnoPangolin <aewers@ucsc.edu>
2025-04-02 18:26:20 -0700

    Completed code journal session 1

---
+++ CJ1_p1.py
@@ -0,0 +1,12 @@
+'''
+Title: CJ1_p1.py
+Author: April Ewers
+Date: 2025-04-02
+Assignment: Code Journal #1 Prompt #1
+Task: Print your name, pronouns, and sentences about favorite movie and food.
+'''
+
+print('April Ewers')
+print('She/her')
+print("I'm a big fan of The Wind Rises, a Ghibli movie that is a beautiful exploration of obsession and alienation.")
+print("I love bread, because I can be sure to give everyone around great joy by baking it for them.")
